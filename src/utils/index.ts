8fbcf628bd7c64cbf89f11c009e7c3193732f865
FLomka <flomkal@gmail.com>
2024-06-12 22:15:59 +0300

    v1.0.0

---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export * as isMobileDevice from "./isMobileDevice"
---
+++ src/utils/isMobileDevice/index.ts
@@ -0,0 +1,17 @@
+"use server"
+
+import {headers} from "next/headers"
+import {UAParser} from "ua-parser-js"
+
+export default async function isMobileDevice() {
+	if (typeof process === "undefined") {
+		throw new Error("[Server method] you are importing a server-only module outside of server")
+	}
+
+	const {get} = headers()
+	const ua = get("user-agent")
+
+	const device = new UAParser(ua || "").getDevice()
+
+	return device.type === "mobile"
+}
